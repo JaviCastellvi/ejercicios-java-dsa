99d1e01e3632aa918073d704750fc55d06789a0b
JaviCastellvi <javiderave@gmail.com>
2015-02-20 17:44:53 +0100

    first

---
+++ src/main/java/edu/upc/eetac/dsa/javicastellvi/ejercicios_java_dsa/App.java
@@ -0,0 +1,13 @@
+package edu.upc.eetac.dsa.javicastellvi.ejercicios_java_dsa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
